9bdeb8ebf5c0b84b4c3c3db7087a0391ffe79e62
William M. Tsikata <tsikatawill@gmail.com>
2022-11-19 22:07:56 +0000

    feat: add mock news items for news reel

---
+++ utils/data.ts
@@ -0,0 +1,39 @@
+import { NewsReelItemI } from "./../types/index";
+export const NewsReelItems: NewsReelItemI[] = [
+  {
+    title: "Some news",
+    summary:
+      "Nisi nemo in, aliquid tempora quis magni mollitia excepturi sequi voluptatum, ratione libero? Ad dolore modi rerum repellat sint perspiciatis, id necessitatibus.",
+    image: "/images/LT.jpg",
+  },
+  {
+    title: "Other news",
+    summary:
+      "Nisi nemo in, aliquid tempora quis magni mollitia excepturi sequi voluptatum, ratione libero? Ad dolore modi rerum repellat sint perspiciatis, id necessitatibus.",
+    image: "/images/CoDE.jpg",
+  },
+  {
+    title: "Further news",
+    summary:
+      "Nisi nemo in, aliquid tempora quis magni mollitia excepturi sequi voluptatum, ratione libero? Ad dolore modi rerum repellat sint perspiciatis, id necessitatibus.",
+    image: "/images/Supper.jpg",
+  },
+  {
+    title: "Some other news",
+    summary:
+      "Nisi nemo in, aliquid tempora quis magni mollitia excepturi sequi voluptatum, ratione libero? Ad dolore modi rerum repellat sint perspiciatis, id necessitatibus.",
+    image: "/images/LT.jpg",
+  },
+  {
+    title: "More news",
+    summary:
+      "Nisi nemo in, aliquid tempora quis magni mollitia excepturi sequi voluptatum, ratione libero? Ad dolore modi rerum repellat sint perspiciatis, id necessitatibus.",
+    image: "/images/CoDE.jpg",
+  },
+  {
+    title: "Further newss",
+    summary:
+      "Nisi nemo in, aliquid tempora quis magni mollitia excepturi sequi voluptatum, ratione libero? Ad dolore modi rerum repellat sint perspiciatis, id necessitatibus.",
+    image: "/images/Supper.jpg",
+  },
+];
