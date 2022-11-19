a747a94e784f4d69976c1392f81e290e410b17c3
William M. Tsikata <tsikatawill@gmail.com>
2022-11-19 22:06:58 +0000

    chore: add footer that sticks to bottom of page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import { useContext } from "react";
-import { Container, Navbar, Text, Block, Button } from "../components";
+import { Navbar, Text, Block, NewsReel, Footer } from "../components";
 
 import { ThemeContext } from "../providers/ThemeProvider";
 import { darkTheme, styled } from "../stitches.config";
+import { NewsReelItems } from "../utils/data";
 
 export default function Home() {
   const { theme } = useContext(ThemeContext);
@@ -15,31 +16,20 @@ export default function Home() {
         <meta name="description" content="UCC portal" />
         <link rel="icon" href="/images/ucc-logo.gif" />
       </Head>
+      <Block dFlex direction="column" css={{ minHeight: "100vh" }}>
+        <Navbar />
 
-      <Navbar />
+        <main>
+          <NewsReel newsItems={NewsReelItems} />
+        </main>
 
-      <main>
-        <Container>
-          <Block dFlex align="center">
-            <Text css={{ flex: 1 }}>
-              commodi error quae sapiente obcaecati ad, exercitationem adipisci,
-              eum harum voluptate vero qui atque!
-            </Text>
-            <Text css={{ flex: 1 }}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Doloremque fugit id magnam pariatur dicta, saepe temporibus
-              commodi error quae sapiente obcaecati ad, exercitationem adipisci,
-              eum harum voluptate vero qui atque!
-            </Text>
-          </Block>
-        </Container>
-      </main>
+        <Footer />
+      </Block>
     </Wrapper>
   );
 }
 
 const Wrapper = styled("div", {
-  minHeight: "100vh",
   background: "$primaryBg",
   color: "$primaryText",
 });
